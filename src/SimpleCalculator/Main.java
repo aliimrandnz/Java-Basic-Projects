c3f9369870f527698f1682a22602d62250bb4c3b
Ali Deniz <aliimrandnz@gmail.com>
2026-01-01 01:21:53 -0500

    Project 3: Simple Calculator Completed With Switch Case.

---
+++ src/SimpleCalculator/Main.java
@@ -0,0 +1,49 @@
+package SimpleCalculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double firstNumber = input.nextDouble();
+        System.out.print("Enter the second number: ");
+        double secondNumber = input.nextDouble();
+        System.out.print("Choose an operator by numbers: ");
+        System.out.print("The operators are (Addition = 1) (Substraction = 2) (Multiplication = 3) (Division = 4) ");
+        int selectedOperator = input.nextInt();
+
+        double result = 0;
+
+        switch (selectedOperator) {
+
+            case 1:
+                result = firstNumber + secondNumber;
+                System.out.println("Result is: " + result);
+                break;
+
+            case 2:
+                result = firstNumber - secondNumber;
+                System.out.println("Result is: " + result);
+                break;
+
+            case 3:
+                result = firstNumber * secondNumber;
+                System.out.println("Result is: " + result);
+                break;
+
+            case 4:
+                if (secondNumber == 0) {
+                    System.out.println("Cannot Divide to Zero.");
+                    break;
+                }
+                result = firstNumber / secondNumber;
+                System.out.println("Result is: " + result);
+                break;
+
+            default:
+                System.out.println("Invalid choose!");
+        }
+
+    }
+}
