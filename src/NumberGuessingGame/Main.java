4e60c4c572701637acd4b55489fe8dd3a5e23e09
Ali Deniz <aliimrandnz@gmail.com>
2026-01-01 14:49:58 -0500

    Small improvements on NumberGuessingGame.

--- src/NumberGuessingGame/Main.java
+++ src/NumberGuessingGame/Main.java
@@ -1,27 +1,27 @@
 package NumberGuessingGame;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int randomNumber = (int)(Math.random()*100)+1;
-        int count =0;
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        int count = 0;
         boolean isFound = false;
-        while(!isFound){
+        while (!isFound) {
             System.out.print("Enter a number between 1-100 (100 included): ");
             int inputNumber = input.nextInt();
             count++;
-            if(inputNumber>randomNumber){
+            if (inputNumber > randomNumber) {
                 System.out.println("Too high! Try again.");
-            }
-            else if(inputNumber<randomNumber){
+            } else if (inputNumber < randomNumber) {
                 System.out.println("Too low! Try again.");
-            }
-            else {
+            } else {
                 System.out.println("You have found the number!");
                 isFound = true;
             }
         }
         System.out.println("You have found the number in " + count + " try!");
+        input.close();
     }
 }
